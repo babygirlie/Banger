3a90aaec048938e98cdcfc3188ed9f97034b703e
Babygirl <sinhasanu59@gmail.com>
2026-06-02 10:29:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ function startExperience() {
     window.location.hash = "s2";
 }
 
-// 3. SURPRISE POP-UP (Page 9)
+// 3. SURPRISE POP-UP (Page 11)
 function showSurprise() {
     const container = document.getElementById('popImg');
     container.classList.add('active');
 }
 
-// 4. CAKE CUTTING & FIREWORKS (Page 10)
+// 4. CAKE CUTTING & FIREWORKS (Page 12)
 function handleCake() {
     const cake = document.getElementById('cakeDisplay');
     const msg = document.getElementById('cakeMsg');
